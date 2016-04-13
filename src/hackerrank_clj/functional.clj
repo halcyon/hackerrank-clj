deefedc4989248837109e1ea495198a5c248d978
Scott McLeod <halcyonblue@gmail.com>
2016-04-13 13:02:14 -0400

    Add filter-odd-positions

--- src/hackerrank_clj/functional.clj
+++ src/hackerrank_clj/functional.clj
@@ -3,8 +3,15 @@
 (defn filter-array []
   (let [in (clojure.string/split (slurp *in*) #"\s")
         delimiter (Integer/parseInt (first in))
-        list_to_sort (map #(Integer/parseInt %) (rest in))]
+        list_to_filter (map #(Integer/parseInt %) (rest in))]
     ((fn[delim lst]
        (dorun (map println
                    (filter #(> delim %1)
-                           lst)))) delimiter list_to_sort)))
+                           lst)))) delimiter list_to_filter)))
+
+(defn filter-odd-positions []
+  (let [in (clojure.string/split (slurp *in*) #"\s")
+        list_to_filter (map #(Integer/parseInt %) in)]
+    ((fn[lst]
+       (dorun (map println
+                   (take-nth 2 (rest lst))))) list_to_filter)))
--- test/hackerrank_clj/functional_test.clj
+++ test/hackerrank_clj/functional_test.clj
@@ -17,3 +17,17 @@
                                     "1\n"
                                     "3\n"
                                     "0\n")))))
+
+(deftest filter-odd-positions-test
+  (is (= (str "5\n"
+              "4\n"
+              "7\n"
+              "8\n")
+         (wrap-io filter-odd-positions (str "2\n"
+                                          "5\n"
+                                          "3\n"
+                                          "4\n"
+                                          "6\n"
+                                          "7\n"
+                                          "9\n"
+                                          "8\n")))))
